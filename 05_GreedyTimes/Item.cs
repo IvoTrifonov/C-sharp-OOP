3c907e8e18cf1f72fda08eaaf0796c2df9cdbcc6
Ivo Trifonov <38018279+IvoTrifonov@users.noreply.github.com>
2019-03-01 11:21:12 +0200

    Add files via upload

---
+++ 05_GreedyTimes/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P05_GreedyTimes
+{
+    public class Item
+    {
+        public Item(string name)
+        {
+            this.Name = name;
+            this.Quantity = 0;
+        }
+
+        public string Name { get; private set; }
+
+        public long Quantity { get; set; }
+    }
+}
